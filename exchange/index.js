facc39ebeae868de0c4347fcdece22af23d645f1
ISHII 2bit <ishii@buffer-renaiss.com>
2018-01-08 03:15:59 +0900

    ignore cache of ticker

--- exchange/index.js
+++ exchange/index.js
@@ -2,7 +2,7 @@ var API = require('../api')
 var UrlPattern = require('url-pattern')
 
 var endpoints = {
-  ticker: new UrlPattern('/ticker(?api_code=:apiCode)'),
+  ticker: new UrlPattern('/ticker(?t=:time)(&api_code=:apiCode)'),
   frombtc: new UrlPattern('/frombtc?value=:value&currency=:currency(&time=:time)(&api_code=:apiCode)'),
   tobtc: new UrlPattern('/tobtc?value=:value&currency=:currency(&api_code=:apiCode)')
 }
@@ -17,7 +17,7 @@ module.exports = {
 
 function getTicker (options) {
   options = options || {}
-  return api.request('ticker', { apiCode: options.apiCode })
+  return api.request('ticker', { apiCode: options.apiCode, time: +(new Date()) })
     .then(function (data) { return data[options.currency] || data })
 }
 
